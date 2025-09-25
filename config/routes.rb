ca2e8a9c06a11c404b378d55a3a741737cb5c257
daichi sakai <daichi.fkcx2347@gmail.com>
2025-09-25 03:53:14 +0000

    admin devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  get 'stores/index'
-  get 'stores/show'
-  get 'stores/create'
-  get 'stores/mypage'
-  get 'stores/destroy'
-  get 'stores/update'
-  get 'stores/edit'
-  devise_for :stores
   devise_for :users
+
+  devise_for :stores, controllers: {
+    registrations:  'stores/registrations',
+    sessions: 'stores/sessions'
+  }
+
+  devise_for :admins, controllers: {
+    registrations:  'admins/registrations',
+    sessions: 'admins/sessions'
+  }
+
+  get 'stores/mypage'
   root to: 'homes#top'
   get 'about' => 'homes#about'
-  resources :stores, only: [:mypage, :create, :edit, :index,]
+  resources :stores
   resources :store_images, only: [:new, :index, :show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_09_22_031002) do
+ActiveRecord::Schema.define(version: 2025_09_25_020712) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "store_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
