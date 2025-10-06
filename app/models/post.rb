47b338eaaf58868307878b1ccde3a27044fd4f29
daichi sakai <daichi.fkcx2347@gmail.com>
2025-10-06 02:06:13 +0000

    post_index_bootstrap

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   has_one_attached :post_image
   belongs_to :store
   has_many :post_comments, dependent: :destroy
+
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -7,4 +7,12 @@ class Store < ApplicationRecord
   # has_many :store_images, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_one_attached :store_image
+
+  def get_store_image(width, height)
+    unless store_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      store_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    store_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
