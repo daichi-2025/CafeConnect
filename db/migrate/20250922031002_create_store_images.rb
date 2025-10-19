20ae714c9657e3a803986bde9e00f602879a0929
daichi sakai <daichi.fkcx2347@gmail.com>
2025-10-19 11:23:58 +0000

    デプロイ

--- db/migrate/20250922031002_create_store_images.rb
+++ db/migrate/20250922031002_create_store_images.rb
@@ -1,7 +1,6 @@
 class CreateStoreImages < ActiveRecord::Migration[6.1]
   def change
     create_table :store_images do |t|
-      t.integer :id
       t.integer :store_id
       t.string :caption
       t.timestamps
--- db/migrate/20250925122421_create_posts.rb
+++ db/migrate/20250925122421_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.integer :store_id
       t.string :title, null: false, default:""
-      t.text :body, null: false, default:""
+      t.text :body, null: false
       t.text :comment
       t.datetime :remember_created_at
       t.timestamps null: false
