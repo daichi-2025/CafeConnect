d094fed54e10c5851066bd2d002eeae95edd6279
daichi sakai <daichi.fkcx2347@gmail.com>
2025-10-28 04:05:44 +0000

    favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,27 @@
 class FavoritesController < ApplicationController
   def create
     store = Store.find(params[:store_id])
-    favorite = current_user.favorites.new(store_id: store.id)
-    favorite.save
-    redirect_to request.referer
+    if current_user
+      favorite = current_user.favorites.new(store_id: store.id)
+      favorite.save
+    elsif current_store
+      favorite = current_store.favorites.new(store_id: store.id)
+      favorite.save
+    else
+      redirect_to request.referer
+    end
   end
   
   def destroy
     store = Store.find(params[:store_id])
-    favorite = current_user.favorites.find_by(store_id: store.id)
-    favorite.destroy
-    redirect_to request.referer
+    if current_user
+      favorite = current_user.favorites.find_by(store_id: store.id)
+      favorite.destroy
+    elsif urrent_store
+      favorite = current_store.favorites.find_by(store_id: store.id)
+     favorite.destroy
+    else
+      redirect_to request.referer
+    end
   end
 end
