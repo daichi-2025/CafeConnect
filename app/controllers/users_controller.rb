f3f8d106dc53f8f766b6a2fc6ce2a8f6fc33bfc8
daichi sakai <daichi.fkcx2347@gmail.com>
2025-11-09 08:17:07 +0000

    fix user_destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(current_user.id)  
     user.destroy  
-    redirect_to new_user_registration_path
+    redirect_to top_path
   end
 
   def update
