36bf1c1b8856b753f2fed0173c33de94d07cb506
daichi sakai <daichi.fkcx2347@gmail.com>
2025-11-16 03:22:32 +0000

    user_likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,13 @@ class UsersController < ApplicationController
     @store = Store.find(params[:id])
   end
 
+  def favorites 
+    @user = User.find(params[:id])
+    likes = Like.where(user_id: @user.id).pluck(:post_id)
+    @like_posts = Post.find(likes)
+    @post = Post.find(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(:user_image, :name, :email)    
