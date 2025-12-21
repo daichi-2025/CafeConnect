3b567f18df7e6e3941a75e15023cac21964f3a4d
daichi sakai <daichi.fkcx2347@gmail.com>
2025-12-21 04:54:18 +0000

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# デプロイを学ぼう 7章【テストデータを理解しよう】 参照
-
 Daichi = User.find_or_create_by!(email: "daichi@example.com") do |user|
   user.name = "daichi"
   user.password = "password"
