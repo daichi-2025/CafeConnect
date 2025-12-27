f4ad539b62013670df48648da280de174a37eb6a
daichi sakai <daichi.fkcx2347@gmail.com>
2025-12-27 03:24:52 +0000

    fix post_comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -7,8 +7,17 @@ class PostCommentsController < ApplicationController
       comment = current_store.post_comments.new(post_comment_params)
     end
     comment.post_id = post.id
-    comment.save!
-    redirect_to post_path(post)
+
+    if comment.save
+      redirect_to post_path(post), notice: 'コメントが投稿されました。'
+    else
+      # 保存失敗時は投稿詳細に戻りつつ、エラーメッセージを表示したい場合
+      @post = post
+      # 例えば新たにコメント投稿用のインスタンス変数も用意しておく
+      @post_comment = comment
+      flash.now[:alert] = 'コメントを入力してください。'
+      render 'posts/show' # 投稿詳細画面をレンダリング(ビュー名は適宜調整してください)
+    end
   end
 
   def destroy
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,4 +2,6 @@ class PostComment < ApplicationRecord
   belongs_to :post
   belongs_to :user, optional: true
   belongs_to :store, optional: true
+
+  validates :comment, presence: true
 end
